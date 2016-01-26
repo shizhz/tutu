79fbafc33a3d74980c8f44d6ea766442d516e94e
shizhz <messi.shizz@gmail.com>
2016-01-26 22:59:14 +0800

    Select the only suggestion when hit enter key

--- static/js/tutu.js
+++ static/js/tutu.js
@@ -1,9 +1,20 @@
 var Tutu = Tutu || (function() {
+    function removeSuggestionMenu() {
+        var cmdSegs = $('#mesage-input').val().split();
+        var suggestions = $('div.tt-menu div.tt-suggestion>strong');
+        if(suggestions.length == 1) {
+            var lastCmdSeg = cmdSegs[cmdSegs.length - 1];
+            var suggestion = lastCmdSeg == suggestions.text();
+            // TODO: select it
+        }
+    }
+
     function initHotkeyBindings() {
         var msg_box = $('#message-input')[0];
 
         Mousetrap(msg_box).bind('enter', function(e) {
             var suggestion_menu_visible = $('div.tt-menu').is(":visible");
+            removeSuggestionMenu();
             if (suggestion_menu_visible) {
                 console.log("menu is visible, ignore logic here");
                 return false;
