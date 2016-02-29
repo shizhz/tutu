e439460bad4098db90c2aa6b75f18700fa409f78
shizhz <zhenzhong@igola.com>
2016-02-29 15:38:54 +0800

    Marathon address resolving logic

--- modules/mesos/marathon.py
+++ modules/mesos/marathon.py
@@ -1,1 +1,32 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function
+
+import kazoo.client
+import kazoo.exceptions
+import requests
+import requests.exceptions
+
+from .. import log
+
+from . import zookeeper
+
+class MarathonResolver(object):
+    def __init__(self, zk):
+        self.zk = zk
+
+    def resolve(self):
+        hosts, path = self.zk[5:].split("/", 1)
+        path = "/" + path + "/leader"
+
+        with zookeeper.client(hosts=hosts, read_only=True) as zk:
+            try:
+                marathon_addresses = map(lambda path: client.get(path)[0], map(lambda n: '/marathon-cluster/leader/' + n, zk.get_children(path)))
+            except kazoo.exceptions.NoNodeError:
+                log.fatal(INVALID_PATH.format(cfg))
+
+            return marathon_addresses
+
+class MarathonMaster(object):
+    def __init__(self, addresses):
+        self.addresses = addresses
