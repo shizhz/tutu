f323e837e59cc3e515bbfc2d4e7889d5d63acb05
shizhz <messi.shizz@gmail.com>
2016-03-07 22:32:49 +0800

    Remove env option for sh command

--- modules/command/cmd_sh.py
+++ modules/command/cmd_sh.py
@@ -4,10 +4,11 @@ import paramiko
 import contextlib
 from paramiko.client import SSHClient
 import logging
+import itertools
 
 from models import CommandInfo
 from cmd import Command, CommandContext, validator, cmd_indicator
-from modules.mesos.marathon import marathon_of_env
+from modules.mesos.marathon import marathons
 from config.config import PUBKEY_LOCATION
 
 logger = logging.getLogger('tutu.modules.command.' + __name__)
@@ -32,24 +33,22 @@ class ShCommand(Command):
     """
     NAME: sh - execute shell command within application container on remote machine
 
-    SYNOPSIS: sh [--env|-e <ENV>] <marathin_app_id> <raw bash command>
+    SYNOPSIS: sh <marathin_app_id> <raw bash command>
 
     DESC: execute shell within the specified application container and get the result, the target container will be found by marathon_app_id.\n
 
-    You can also optionally specify target `ENV` by --env or -e
 
     NOTE: Please DO NOT use commands which never terminate automatically. like `tail -f`
 
     E.G. sh dev-some-service ls /opt/logs
          sh dev-some-service tail 200 /opt/logs/out.log
-         sh --env dev some-service tail 200 /opt/logs/out.log
-         sh -e prod some-service tail 200 /opt/logs/out.log
+         sh some-service tail 200 /opt/logs/out.log
+         sh some-service tail 200 /opt/logs/out.log
     """
     name = 'sh'
     alias = ['shell', 'bash']
 
-    def __init__(self, env, marathon_app_id, sh):
-        self.env = env
+    def __init__(self, marathon_app_id, sh):
         self.marathon_app_id = marathon_app_id
         self.sh = sh
         self.notification = ""
@@ -89,11 +88,10 @@ class ShCommand(Command):
                 {1}""".format(self.cmd(), '\n\t'.join(err))
 
     def execute(self):
-        marathon = marathon_of_env(self.env)
-        apps = marathon.apps_by_id_contains(self.marathon_app_id)
+        apps = itertools.chain.from_iterable(map(lambda marathon: marathon.apps_by_id_contains(self.marathon_app_id), marathons))
 
         if len(apps) > 1:
-            return 'More than one app found: {0}. Please choose the right one.'.format(map(lambda app: app.id, apps))
+            return 'More than one app found: {0}. Please choose the one you are interested.'.format(', '.join(map(lambda app: app.id, apps)))
 
         marathon_app = apps[0]
         return '\n'.join(map(lambda t: self.exec_sh_on_task(marathon_app, t), marathon_app.tasks))
@@ -101,47 +99,21 @@ class ShCommand(Command):
 class ShCommandParser(object):
     support = cmd_indicator(ShCommand)
 
-    def has_env_option(self, txt):
-        cmd_segs = txt.split()
-        return '-e' == cmd_segs[1].lower() or '--env' == cmd_segs[1].lower()
-
-    def parse_env(self, txt):
-        cmd_segs = txt.split()
-
-        def parse_by_arg():
-            if self.has_env_option(txt):
-                return cmd_segs[2]
-
-        def parse_by_app_id():
-            app_id_index = 1 + (2 if self.has_env_option(txt) else 0)
-            app_id = cmd_segs[app_id_index]
-            return app_id.split('-')[0]
-
-        return parse_by_arg() or parse_by_app_id()
-
     def parse_marathon_app_id(self, txt):
-        cmd_segs = txt.split()
-
-        return cmd_segs[3] if self.has_env_option(txt) else cmd_segs[1]
+        return txt.split()[1]
 
     def parse_remote_sh(self, txt):
-        cmd_segs = txt.split()
-
-        return cmd_segs[4:] if self.has_env_option(txt) else cmd_segs[2:]
+        return txt.split()[2:]
 
     @validator
     def is_valid(self, txt):
         cmd_segs = txt.split()
-        return self.support(txt) and (len(cmd_segs) >= 5 if self.has_env_option(txt) else len(cmd_segs) >= 3)
+        return self.support(txt) and len(cmd_segs) >= 3
 
     def parse(self, txt):
         """
         sh command should be the format like:
         - sh dev-some-service ls /opt/logs
         - sh dev-some-service tail -f /opt/logs/out.log
-        - sh -e dev some-service tail -f /opt/logs/out.log
         """
-        env = self.parse_env(txt)
-        marathon_app_id = self.parse_marathon_app_id(txt)
-        sh = self.parse_remote_sh(txt)
-        return ShCommand(env, marathon_app_id, sh)
+        return ShCommand(txt.split()[1], txt.split(2:))
