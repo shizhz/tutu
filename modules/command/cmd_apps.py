2d024921ae0f4c26830b1319b341657104a04b74
shizhz <zhenzhong@igola.com>
2016-03-08 18:01:26 +0800

    Add basic pattern match(^<app_id_keyword> for startswith, <app_id_keyword>$ for endswith) for appinfo command

--- modules/command/cmd_apps.py
+++ modules/command/cmd_apps.py
@@ -105,9 +105,21 @@ class AppInfoCommand(Command):
  Task info: {task_info}
  Container info: {c_info} """.format(app.id, c_info=app.container_info(verbose=False), task_info=app.task_info(), a_addr=app.str_bamboo_address(), api_gateway=app.str_api_gateway_address())
 
+    def find_apps(self):
+        def filter_by_kw(kw):
+            def app_id_contains(app):
+                return kw in app.id
+            def app_id_endswith(app):
+                return app.id.endswith(kw[:-1])
+            def app_id_startswith(app):
+                return app.id.startswith(kw[1:])
+
+            return app_id_endswith if kw.endswith('$') else app_id_startswith if kw.startswith('^') else app_id_contains
+
+        return filter(lambda app: any(map(lambda ta: filter_by_kw(ta)(app), self.target_apps)), itertools.chain.from_iterable(map(Marathon.apps, marathons)))
 
     def execute(self):
-        apps = filter(lambda app: any(map(lambda ta: ta in app.id, self.target_apps)), itertools.chain.from_iterable(map(Marathon.apps, marathons)))
+        apps = self.find_apps()
 
         if len(apps) == 0:
             return "No App found."
